ca1f4ffcdf55cd053c2955a0c5912b99f1ea52e6
Jonatan Gelerstain <jonatangeler7@gmail.com>
2024-12-24 11:53:26 +0200

    Comments for code

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-#create table
+#create User שם משתמש 
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
@@ -30,6 +30,7 @@ class User(db.Model):
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="user", lazy="select")
 
+#create Task יצירת משימה
 class Task(db.Model):
     id: Mapped[int] = db.Column(db.Integer, primary_key=True)  
     description: Mapped[str] = db.Column(db.Text, nullable=False)  
@@ -45,28 +46,33 @@ with app.app_context():
     db.create_all()
 
 
+#דף בית התחברות והרשמה
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#דף התחברות, שם משתמש וסיסמא עם כפתור submit
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         
+        #בודק במאגר שיש שם משתמש כזה ושהסיסמא תואמת
         user = User.query.filter_by(username=username).first()
         if not user or not check_password_hash(user.password, password):
             flash('Invalid username or password!', 'danger')
             return redirect(url_for('login'))
 
+        #יוצר את ה session 
         session['user_id'] = user.id
 
-        # flash('Login successful!', 'success')
+        #הצלחה עובר לדף של אותו משתמש ולמשימות שלו
         return redirect(url_for('todo'))
     
     return render_template('login.html')
 
+#דף הרשמה, שם מתמש סיסמא וסיסמא שניה כבדיקה שהיא תואמת
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -74,16 +80,20 @@ def register():
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
 
+        #בדיקת הסיסמאות
         if password != confirm_password:
             flash('Passwords do not match!', 'danger')
             return redirect(url_for('register'))
 
+        #בדיקה שאין שם משתמש כזה כי זה יחודי
         if User.query.filter_by(username=username).first():
             flash('Username already exists!', 'danger')
             return redirect(url_for('register'))
 
+        #הופך את הסיסמא ל hash 
         hashed_password = generate_password_hash(password)
         new_user = User(username=username, password=hashed_password)
+        #הכנסה ל data base
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful!', 'success')
@@ -91,6 +101,8 @@ def register():
 
     return render_template('register.html')
 
+#דף משימות וחיבור לאותו שם משתמש
+#כל המשימות שלנו ואפשרות להוסיף משימות (כפתורים שעוזרים לנהל את המשימות)
 @app.route('/todo', methods=['GET', 'POST'])
 def todo():
     if 'user_id' not in session:
@@ -100,10 +112,11 @@ def todo():
     user_id = session['user_id']
     user = User.query.get(user_id)
 
+    #הוספת המטלה לdata base השני עם חיבור לאותו ID  של המשתמש 
     if request.method == 'POST':
         description = request.form.get('description')
-        date = request.form.get('date')  # קבלת תאריך מהטופס
-        time = request.form.get('time')  # קבלת שעה מהטופס
+        date = request.form.get('date')  
+        time = request.form.get('time')  
 
         new_task = Task(
             description=description,
@@ -119,6 +132,7 @@ def todo():
     tasks = Task.query.filter_by(user_id=user_id).all()
     return render_template('todo.html', user=user, tasks=tasks)
 
+#שינוי המטלה במקרה שיש לשנות משהו
 @app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
     task = Task.query.get(task_id)
@@ -141,6 +155,7 @@ def edit_task(task_id):
 
     return render_template('edit_task.html', task=task)
 
+#ביטול המשימה במקרה שלא השלמנו ולחצנו על השלמה 
 @app.route('/undo_task/<int:task_id>', methods=['POST'])
 def undo_task(task_id):
     task = Task.query.get(task_id)
@@ -153,6 +168,7 @@ def undo_task(task_id):
     #flash('Task marked as not done!', 'success')
     return redirect(url_for('todo'))
 
+#מחיקת משימה
 @app.route('/delete_task/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
     task = Task.query.get(task_id)
@@ -165,6 +181,7 @@ def delete_task(task_id):
     #flash('Task deleted successfully!', 'success')
     return redirect(url_for('todo'))
 
+#סימון השם של המשימה בקו אחרי השלמה 
 @app.route('/mark_task/<int:task_id>', methods=['POST'])
 def mark_task(task_id):
     task = Task.query.get(task_id)
@@ -174,6 +191,7 @@ def mark_task(task_id):
         # flash('Task marked as done!', 'success')
     return redirect(url_for('todo'))
 
+#יצירה מהמשתמש
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
